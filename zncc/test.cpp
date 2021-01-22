97eeebbdb8a8348d5bf8b1d8eadbdfce8f50d37c
Edoardo-Putti <33325681+Edoardo-Putti@users.noreply.github.com>
2021-01-22 09:45:33 +0100

    latest

--- zncc/test.cpp
+++ zncc/test.cpp
@@ -1,49 +1,120 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "signals.h"
+#include <math.h>
+#include "ap_int.h"
+#include <hls_stream.h>
 
-#define ROW 440
-#define COL 440
+#define IMG_SIDE 520
+#define TMP_SIDE 80
+#define RES_SIDE (IMG_SIDE-TMP_SIDE+1)
 
+struct data_struct{
+  float data;
+  bool last;
+};
 
-void zncc_hd(float* img, float* templ, float* res, bool first, float B, float D);
+void zncc(ap_uint<512> *mask,ap_uint<512> *t, hls::stream<data_struct> &out, float b, float d, bool new_row, bool init, bool end);
 
 int main(){
     bool correct = true;
-    bool first = true;
-    float res[440*440];
+    bool new_row = true;
+    bool setup = true;
+    bool end = false;
+    float res[440][440];
     float* img=image;
     float* t=templ;
     float B = b;
     float D = d;
-    float mask[80*80];
-    float col[80];
-    printf("tst b: %f, d: %f",B,D);
+    float mask[TMP_SIDE*TMP_SIDE];
+    float col[TMP_SIDE];
+    float out[0];
+    hls::stream<data_struct> stream_out("stream_out");
+    float ka =666;
+/*
+    	ap_uint<512> test[25];
+        float vec[160];
+        for(int x=0;x<160;x++){
+            vec[x]=(float)x;
+        }
+        float ta[80];
+        memcpy(test,vec,sizeof(ap_uint<512>)*25);
+        memcpy(ta,test,sizeof(ap_uint<512>)*5);
 
-    for(int r=0; r<ROW; ++r){
-        first = true;
-        for(int x=0; x<80;++x){
-            for(int y=0; y<80;++y){
-                mask[x*80+y] = img[x*520+520*r+y];
+        for(int x=0;x<80;x++){
+        	printf("%f\n",ta[x]);
+        }
+
+        for(int r=0;r<2;r++){
+        	unsigned int tmp = test[r*5].range(63,32);
+        	float tmp_val = *((float *)&tmp);
+        	printf("--> %f \n",tmp_val);
+        	int k=0;
+            for(int c=0;c<5;c++){
+
+            	for(int j=1;j<16;j++){
+
+					int upper = (j)*32-1;
+					int lower = (j-1)*32;
+					int upper2 = (j+1)*32-1;
+					int lower2 = j*32;
+
+					//unsigned int m_0 = lmask[r*N_AP+c].range(upper,lower);
+					unsigned int m_1 = test[r*5+c].range(upper2,lower2);
+
+					//float m0 = *((float *)&m_0);
+					float m1 = *((float *)&m_1);
+
+					test[r*5+c].range(upper,lower) = m_1;
+            	}
+            	unsigned int m_1 = test[r*5+c+1].range(31,0);
+            	test[r*5+c].range(511,480) = m_1;
+            	//test[r*5+4].range(511,480) = *((unsigned int *)&ka);
+            	for(int j=0;j<16;j++){
+
+                    int up = (j+1)*32-1;
+                    int low = j*32;
+                    unsigned int t = test[r*5+c].range(up,low);
+                    float t_val = *((float *)&t);
+                    printf("%d %d : %f \n",r,k,t_val);
+                    k++;
+                }
             }
         }
-        zncc_hd(mask,t,res,first,B,D);
-        for(int c=0; c<COL;++c){
-            first=false;
-            for(int x=0; x<80;++x){
-                col[x]=img[x*520+520*r+80+c];
+*/
+    for(int r=0; r<RES_SIDE; ++r){
+        new_row = true;
+        for(int x=0; x<TMP_SIDE;++x){
+            for(int y=0; y<TMP_SIDE;++y){
+                mask[x*TMP_SIDE+y] = img[(r+x)*IMG_SIDE+y];
             }
-            zncc_hd(col,t,res,first,B,D);
-            
+        }
+
+        zncc((ap_uint<512>*)mask,(ap_uint<512>*)t,stream_out,B,D,new_row,setup,end);
+        float tmp = stream_out.read().data;
+        //res[r][0]=out[0];
+        res[r][0]=tmp;
+        //printf("%f\n",res[r][0]);
+        setup=false;
+        for(int c=1; c<RES_SIDE;++c){
+            new_row=false;
+            for(int x=0; x<TMP_SIDE;++x){
+                col[x]=img[x*IMG_SIDE+IMG_SIDE*r+TMP_SIDE+(c-1)];
+            }
+
+            zncc((ap_uint<512>*)col,(ap_uint<512>*)t,stream_out,B,D,new_row,setup,end);
+            res[r][c]=stream_out.read().data;
+            //res[r][c]=out[0];
+            printf("[%d,%d]-> %f\n",r,c,res[r][0]);
         }
     }
 
-   
+
     
-    
-    for(int x=0; x<ROW; ++x){
-        for(int y=0; y<COL; ++y){
-            if(res[x*COL+y]!=gold[x*COL+y]){
+    for(int x=0; x<RES_SIDE-1 && correct; ++x){
+        for(int y=0; y<RES_SIDE-1 && correct; ++y){
+            if(abs(res[x][y])-abs(gold[x*RES_SIDE+y]) >0.0001){
+            	printf("pos %d,%d res: %f | gold: %f\n",x,y,res[x*RES_SIDE+y],gold[x*RES_SIDE+y]);
                 correct=false;
             }
         }
--- zncc/zncc.cpp
+++ zncc/zncc.cpp
@@ -1,130 +1,155 @@
-#include <math.h>
-#include <stdio.h>
-#include <stdlib.h>
-
+#include "string.h"
+#include "ap_int.h"
+#include "math.h"
+#include <hls_stream.h>
 
 #define ROW 80
 #define COL 80
-#define N ROW*COL
+#define N ((ROW*COL)/16)
+#define N_AP (ROW/16)
 
-void zncc_hd(float* img, float* templ, float* res, bool first, float B, float D){
+struct data_struct{
+  float data;
+  bool last;
+};
+
+
+
+void zncc(ap_uint<512> *mask,ap_uint<512> *t, hls::stream<data_struct> &out, float b, float d, bool new_row, bool init, bool end){
+//interfaces for the axi master communication
+#pragma HLS INTERFACE m_axi depth=6400 port=mask bundle=gmem0
+#pragma HLS INTERFACE m_axi depth=6400 port=t bundle=gmem1
+#pragma HLS INTERFACE m_axi depth=6400 port=res bundle=gmem2
+
+//interfaces for the addresses of the variables
+#pragma HLS INTERFACE s_axilite port=mask
+#pragma HLS INTERFACE s_axilite port=t
+#pragma HLS INTERFACE s_axilite port=res
+#pragma HLS INTERFACE s_axilite port=b
+#pragma HLS INTERFACE s_axilite port=d
+//interface for start/stop the kernel
 #pragma HLS INTERFACE s_axilite port=return
-#pragma HLS INTERFACE s_axilite port=D
-#pragma HLS INTERFACE s_axilite port=B
-#pragma HLS INTERFACE s_axilite port=first
-#pragma HLS INTERFACE m_axi depth=6400 port=templ
-#pragma HLS INTERFACE m_axi depth=6400 port=img
-#pragma HLS INTERFACE m_axi depth=6400 port=res
+//local variables
+	static ap_uint<512> lmask[N], lt[N];//look for size
+	float lcol[COL];
+	float ld, lb;
+	float txm_acc[16] = {};
+	float sum_acc[16] = {};
+	float pow_acc[16] = {};
+	static float txm=0;
+	static float sum=0;
+	static float pow2=0;
+	static float f_col=0;
+	static float f_pow=0;
+	float score[0];
+	data_struct out_data;
+	ld=d;
+	lb=b; 
+	if(init){
+		//copy the template in the FPGA BRAMs only once for the all process
+		memcpy(lt,t,sizeof(ap_uint<512>)*N);
+	}
+	if(new_row){
+		//copy the new mask this procedure needs to be done for each new row of the img
+		sum=0;
+		pow2=0;
+		txm=0;
+		f_col=0;
+		f_col=0;
+		memcpy(lmask,mask,sizeof(ap_uint<512>)*N);
+		loopRow:for(int r; r<ROW;r++){
+		#pragma HLS PIPELINE
 
-    //static float mask2[ROW][COL]
-    static float mask[ROW][COL];
-    static float templ_buf[ROW][COL];
-    //static float zncc[R][C];
+			unsigned int tmp = lmask[r*N_AP].range(31,0);
+			float tmp_val = *((float *)&tmp);
 
-    //maybe use a single array to store all this values
-    static float mask_pixel_sum=0;
-    static float mask_pixel_square=0;
-    static float prod_m_t=0;
-    static float first_col=0;
-    static float first_col_square=0;
-    static float last_col=0;
-    static float last_col_square=0;
-    static int zncc_index=0;
+			f_col += tmp_val;
+			f_pow += pow(tmp_val,2);
 
-if(first){
-/*
-In these first two loops we save the first column of the mask passed,
-the local mask is initialized and the sums/squares of its pixel are calculated
-*/
-	mask_pixel_sum = 0;
-	mask_pixel_square = 0;
-	prod_m_t = 0;
-	first_col=0;
-	first_col_square=0;
+			loopCol:for(int c; c<N_AP;c++){
+			#pragma HLS PIPELINE
 
-  for(int x=0;x<ROW;++x){
-      first_col += img[x*COL];
-      first_col_square += pow(img[x*COL],2);
-      for(int y=0;y<COL;++y){
-          mask[x][y] = img[x*COL+y];
-          mask_pixel_sum += img[x*COL+y];
-          mask_pixel_square += pow(img[x*COL+y],2);
-      }
-  }
+				loopElem:for(int j=0;j<16;j++){
 
-//the local template is initialized
-   for(int x=0;x<ROW;++x){
-      for(int y=0;y<COL;++y){
-          templ_buf[x][y] = templ[x*COL+y];
-      }
-  }
+					int upper = (j+1)*32-1;
+					int lower = j*32;
 
-//the product pixel for pixel bettween the mask and the template is calculated
-  for(int x=0;x<ROW;++x){
-      for(int y=0;y<COL;++y){
-          prod_m_t += templ_buf[x][y]*mask[x][y];
-      }
-  }
+					unsigned int m_tmp = lmask[r*N_AP+c].range(upper,lower);
+					unsigned int t_tmp = lt[r*N_AP+c].range(upper,lower);
 
-/*
-in case there are still resources available we can duplicate the mask and start the
-shifting so when the next data are ready we just need to add the new column
+					float m_val = *((float *)&m_tmp);
+					float t_val = *((float *)&t_tmp);
 
-    for(int x=0;x<80;++x){
-        for(int y=0;y<80;++y){
-            mask2[x][y]= img[x*COL+y];
-        }
-    }
+					txm_acc[j]+= t_val*m_val;
+					sum_acc[j]+= m_val;
+					pow_acc[j]+= pow(m_val,2);
 
-    for(int r = 0; r<ROW; ++r){
-        for(int c=1;c<COL;++c){
-            mask2[r][c-1]=mask2[r][c];
-        }
-    }
-*/
-}
-else{
-    //shift all elements
-    for(int r = 0; r<ROW; ++r){
-        for(int c=1;c<COL;++c){
-            mask[r][c-1]=mask[r][c];
-        }
-    }
-    mask_pixel_sum -= first_col;
-    mask_pixel_square -= first_col_square;
-    first_col=0;
-    first_col_square=0;
-    last_col=0;
-    last_col_square=0;
-    //add new column and calculate new first and last col
-    for(int r =0; r<ROW;++r){
-        mask[r][COL-1]=img[r];
-        last_col += img[r];
-        last_col_square += pow(img[r],2);
-        first_col += mask[r][0];
-        first_col_square += pow(mask[r][0],2);
-    }
+				}
+			}
+		}
+		for(int i=0;i<16;i++){
+		#pragma HLS PIPELINE
+			sum += sum_acc[i];
+			pow2 += pow_acc[i];
+			txm += txm_acc[i];	
+		}
+	}
+	else{
+		sum -= f_col;
+		pow2 -= f_pow;
+		txm=0;
+		f_col=0;
+		f_pow=0;
 
-    //calculate the new sum/square of the mask
-    mask_pixel_sum += last_col;
-    mask_pixel_square += last_col_square;
+		memcpy(lcol,mask,sizeof(ap_uint<512>)*N_AP);
 
-    /*the product pixel for pixel bettween the mask and the template is
-    first set to zero in order to calculate the one with the new mask
-    */
-    prod_m_t = 0;
-    for(int x=0;x<ROW;++x){
-        for(int y=ROW;y<COL;++y){
-            prod_m_t += templ_buf[x][y]*mask[x][y];
-        }
-    }
-    mask_pixel_sum -= first_col;
-    mask_pixel_square -= first_col_square;
+		for(int r=0;r<ROW;r++){
 
-}
+			unsigned int tmp = lmask[r*N_AP].range(63,32);
+			float tmp_val = *((float *)&tmp);
 
-//save back the result for this mask
-res[zncc_index] = ((N*prod_m_t)-(B*mask_pixel_sum))/(D*(sqrt((N*mask_pixel_square - pow(mask_pixel_sum,2)))));
-zncc_index++;
+			f_col += tmp_val;
+			f_pow += pow(tmp_val,2);
+			sum += lcol[r];
+			pow2 += pow(lcol[r],2);
 
+			for(int c=0;c<N_AP;c++){
+			#pragma HLS PIPELINE
+				for(int j=1;j<16;j++){
+
+					int upper = (j)*32-1;
+					int lower = (j-1)*32;
+					int upper2 = (j+1)*32-1;
+					int lower2 = j*32;
+
+					unsigned int m_1 = lmask[r*N_AP+c].range(upper2,lower2);
+					unsigned int t_tmp = lt[r*N_AP+c].range(upper,lower);
+
+					float m_val = *((float *)&m_1);
+					float t_val = *((float *)&t_tmp);
+
+					txm_acc[j]+= t_val*m_val;
+					//shift
+					lmask[r*N_AP+c].range(upper,lower) = m_1;
+
+				}
+				unsigned int m_1 = lmask[r*N_AP+c+1].range(31,0);
+				lmask[r*N_AP+c].range(511,480) = m_1;
+				lmask[r*N_AP+4].range(511,480)= *((unsigned int *)&lcol[r]);
+				unsigned int t_tmp =lt[r*N_AP+4].range(511,480);
+				float t_val = *((float *)&t_tmp);
+				txm += t_val*lcol[r];
+			}
+			
+		}
+		for(int i=0;i<16;i++){
+		#pragma HLS PIPELINE
+			txm += txm_acc[i];	
+		}
+	}
+	out_data.data=((ROW*COL*txm)-(b*sum))/(d*(sqrt((ROW*COL*pow2 - pow(sum,2)))));
+	out_data.last=end;
+	out.write(out_data);/*
+	score[0]=((ROW*COL*txm)-(b*sum))/(d*(sqrt((ROW*COL*pow2 - pow(sum,2)))));
+	memcpy(&out[0],&score[0],sizeof(float));*/
 }
