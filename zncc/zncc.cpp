e40e63aa6f6bfc0d345f59b7bbaa5fbda1cad28f
Edoardo-Putti <33325681+Edoardo-Putti@users.noreply.github.com>
2021-02-03 18:33:51 +0100

    seeeeee

--- zncc/zncc.cpp
+++ zncc/zncc.cpp
@@ -5,8 +5,9 @@
 
 #define ROW 80
 #define COL 80
-#define N ((ROW*COL)/16)
-#define N_AP (ROW/16)
+#define N ROW*COL
+
+
 
 struct data_struct{
   float data;
@@ -15,155 +16,247 @@ struct data_struct{
 
 
 
-void zncc(ap_uint<512> *mask,ap_uint<512> *t, hls::stream<data_struct> &out, float b, float d, bool new_row, bool init, bool end){
+
+
+float reducesum(float data[80], float tmp[158]){
+#pragma HLS ALLOCATION instances=FAddSub_fulldsp core
+
+
+
+#pragma INLINE
+    int p3=40;
+    int p1=0;
+    int p2=80;
+
+    /*for(int i=0;i<80;i++){
+    #pragma HLS UNROLL
+        tmp[i]=data[i];
+    }*/
+    memcpy(tmp,data,sizeof(float)*80);
+
+    for(int i=0;i<=5;i++){
+    #pragma HLS UNROLL
+        for(int j=0;j<p3;j++){
+        #pragma HLS UNROLL
+            tmp[j+p2]=tmp[j*2+p1]+tmp[j*2+1+p1];
+
+        }
+        p1=p2;
+        p2+=p3;
+        p3=(p3/2);
+    }
+    return (tmp[154]+tmp[157]);
+};
+
+
+void zncc(ap_uint<512> *mask,ap_uint<512> *t, hls::stream<data_struct> &out, float b, float d, bool new_row, bool init,bool finish){
+//pragmas to reduce the resources utilizations
+#pragma HLS ALLOCATION instances=DDiv core
+#pragma HLS ALLOCATION instances=DSqrt core
+#pragma HLS ALLOCATION instances=fmul limit=2 operation
+#pragma HLS ALLOCATION instances=fadd limit=2 operation
+#pragma HLS ALLOCATION instances=reducesum limit=1 function
+#pragma HLS ALLOCATION instances=FMul_maxdsp core
+#pragma HLS ALLOCATION instances=FAddSub_fulldsp core
 //interfaces for the axi master communication
 #pragma HLS INTERFACE m_axi depth=6400 port=mask bundle=gmem0
-#pragma HLS INTERFACE m_axi depth=6400 port=t bundle=gmem1
-//#pragma HLS INTERFACE m_axi depth=6400 port=res bundle=gmem2
-#pragma HLS INTERFACE axis port=stream_out
+#pragma HLS INTERFACE m_axi depth=6400 port=t bundle=gmem2
+#pragma HLS INTERFACE axis port=out
+#pragma HLS INTERFACE ap_ctrl_none port=return
+
 
 //interfaces for the addresses of the variables
 #pragma HLS INTERFACE s_axilite port=mask
 #pragma HLS INTERFACE s_axilite port=t
-//#pragma HLS INTERFACE s_axilite port=res
 #pragma HLS INTERFACE s_axilite port=b
 #pragma HLS INTERFACE s_axilite port=d
-
+#pragma HLS INTERFACE s_axilite port=finish
+#pragma HLS INTERFACE s_axilite port=new_row
+#pragma HLS INTERFACE s_axilite port=init
 //interface for start/stop the kernel
 #pragma HLS INTERFACE s_axilite port=return
 
+
 //local variables
-	static ap_uint<512> lmask[N], lt[N];//look for size
+	ap_uint<512> tmp1[5];
+#pragma HLS ARRAY_PARTITION variable=tmp1 complete
+	ap_uint<512> tmp2[5];
+#pragma HLS ARRAY_PARTITION variable=tmp2 complete
+	ap_uint<512> tmp3[5];
+#pragma HLS ARRAY_PARTITION variable=tmp3 complete
+
+	float r_sum[158];
+#pragma HLS ARRAY_PARTITION variable=r_sum complete
+	static float lmask[ROW][COL];
+#pragma HLS ARRAY_PARTITION variable=lmask cyclic factor=32 dim=1
+	static float lt[ROW][COL];//
+#pragma HLS ARRAY_PARTITION variable=lt cyclic factor=32 dim=1
 	float lcol[COL];
-	float ld, lb;
-	float txm_acc[16] = {};
-	float sum_acc[16] = {};
-	float pow_acc[16] = {};
+#pragma HLS ARRAY_PARTITION variable=lcol complete
+	float D, B;
+
+	float txm_acc[80];
+#pragma HLS ARRAY_PARTITION variable=txm_acc cyclic factor=10 dim=1
+	float sum_acc[80];
+#pragma HLS ARRAY_PARTITION variable=sum_acc cyclic factor=10 dim=1
+	float pow_acc[80];
+#pragma HLS ARRAY_PARTITION variable=pow_acc cyclic factor=10 dim=1
+
 	static float txm=0;
 	static float sum=0;
 	static float pow2=0;
 	static float f_col=0;
 	static float f_pow=0;
-	//float score[0];
+
 	data_struct out_data;
-	ld=d;
-	lb=b; 
-	if(init){
-		//copy the template in the FPGA BRAMs only once for the all process
-		memcpy(lt,t,sizeof(ap_uint<512>)*N);
-	}
-	if(new_row){
-		//copy the new mask this procedure needs to be done for each new row of the img
-		sum=0;
-		pow2=0;
-		txm=0;
-		f_col=0;
-		f_col=0;
-		memcpy(lmask,mask,sizeof(ap_uint<512>)*N);
-		loopRow:for(int r; r<ROW;r++){
-		//should be unroll to be optimal since row are independent
-		#pragma HLS PIPELINE
+	D=d;
+	B=b;
 
-			unsigned int tmp = lmask[r*N_AP].range(31,0);
-			float tmp_val = *((float *)&tmp);
+if(init){
+	//copy the template in the FPGA BRAMs only once for the all process
+	for(int r=0;r<ROW;r++){
+#pragma HLS PIPELINE
+		std::cout << "start memcpy template" << std::endl;
+		memcpy(&tmp1[0],&t[r*5],sizeof(ap_uint<512>)*5);
+		for(int c=0;c<5;c++){
+//#pragma HLS UNROLL
+			 ap_uint<512> a= tmp1[c];
+			for(int j=0;j<16;j++){
+#pragma HLS UNROLL
+				int upper = (j+1)*32-1;
+				int lower = j*32;
 
-			f_col += tmp_val;
-			f_pow += pow(tmp_val,2);
+				unsigned int t_tmp =a.range(upper,lower);
 
-			loopCol:for(int c; c<N_AP;c++){
-			#pragma HLS PIPELINE
-
-				loopElem:for(int j=0;j<16;j++){
-					//this loop is unrolled completely and the 16 values inside each element of the mask can be computed in parallel
-					int upper = (j+1)*32-1;
-					int lower = j*32;
-
-					unsigned int m_tmp = lmask[r*N_AP+c].range(upper,lower);
-					unsigned int t_tmp = lt[r*N_AP+c].range(upper,lower);
-
-					float m_val = *((float *)&m_tmp);
-					float t_val = *((float *)&t_tmp);
-
-					txm_acc[j]+= t_val*m_val;
-					sum_acc[j]+= m_val;
-					pow_acc[j]+= pow(m_val,2);
-
-				}
-			}
-		}
-		for(int i=0;i<16;i++){
-		#pragma HLS PIPELINE
-			sum += sum_acc[i];
-			pow2 += pow_acc[i];
-			txm += txm_acc[i];	
-		}
-	}
-	else{
-		sum -= f_col;
-		pow2 -= f_pow;
-		txm=0;
-		f_col=0;
-		f_pow=0;
-
-		memcpy(lcol,mask,sizeof(ap_uint<512>)*N_AP);
-
-		for(int r=0;r<ROW;r++){
-
-			unsigned int tmp = lmask[r*N_AP].range(63,32);
-			float tmp_val = *((float *)&tmp);
-
-			f_col += tmp_val;
-			f_pow += pow(tmp_val,2);
-			sum += lcol[r];
-			pow2 += pow(lcol[r],2);
-
-			for(int c=0;c<N_AP;c++){
-			#pragma HLS PIPELINE
-				for(int j=1;j<16;j++){
-
-					int upper = (j)*32-1;
-					int lower = (j-1)*32;
-					int upper2 = (j+1)*32-1;
-					int lower2 = j*32;
-
-					unsigned int m_1 = lmask[r*N_AP+c].range(upper2,lower2);
-					unsigned int t_tmp = lt[r*N_AP+c].range(upper,lower);
-
-					float m_val = *((float *)&m_1);
-					float t_val = *((float *)&t_tmp);
-
-					txm_acc[j] += t_val*m_val;
-					//shift
-					lmask[r*N_AP+c].range(upper,lower) = m_1;
-
-				}
-				//since each cell contains 16 values we need to shift the value of the 17th in the 16th cell
-				//when c=4 we access a value that doesn't exist
-				unsigned int m_1 = lmask[r*N_AP+c+1].range(31,0);
-				float m_val = *((float *)&m_1);
-				unsigned int t_1 = lt[r*N_AP+c].range(511,480);
-				float t_val0 = *((float *)&t_1);
-
-				txm += m_val*t_val0;
-				//actual shift
-				lmask[r*N_AP+c].range(511,480) = m_1;
-
-
-				lmask[r*N_AP+4].range(511,480)= *((unsigned int *)&lcol[r]);
-				unsigned int t_tmp =lt[r*N_AP+4].range(511,480);
 				float t_val = *((float *)&t_tmp);
-				txm += t_val*lcol[r];
+				std::cout << "coping val: "<< t_val << std::endl;
+				lt[r][(c*16)+j]=t_val;
 			}
-			
-		}
-		for(int i=0;i<16;i++){
-		#pragma HLS PIPELINE
-			txm += txm_acc[i];	
 		}
 	}
-	out_data.data=((ROW*COL*txm)-(b*sum))/(d*(sqrt((ROW*COL*pow2 - pow(sum,2)))));
-	out_data.last=end;
-	out.write(out_data);/*
-	score[0]=((ROW*COL*txm)-(b*sum))/(d*(sqrt((ROW*COL*pow2 - pow(sum,2)))));
-	memcpy(&out[0],&score[0],sizeof(float));*/
+	//memcpy(lt,t,sizeof(float)*N);
+
+}
+
+if(new_row){
+	//copy the new mask this procedure needs to be done for each new row of the img
+	sum=0;
+	pow2=0;
+	txm=0;
+	f_col=0;
+	f_pow=0;
+
+	//memcpy(lmask,mask,sizeof(float)*N);
+
+
+	for(int r=0;r<ROW;r++){
+#pragma HLS PIPELINE
+		memcpy(&tmp2[0],&mask[r*5],sizeof(ap_uint<512>)*5);
+		for(int c=0;c<5;c++){
+//#pragma HLS UNROLL
+			ap_uint<512> a=tmp2[c];
+			for(int j=0;j<16;j++){
+#pragma HLS UNROLL
+				int upper = (j+1)*32-1;
+				int lower = j*32;
+
+				unsigned int m_tmp = a.range(upper,lower);
+				float m_val = *((float *)&m_tmp);
+				lmask[r][(c*16)+j]=m_val;
+			}
+		}
+	}
+
+	for(int x=0;x<80;x++){
+#pragma HLS UNROLL factor=10
+		txm_acc[x] = 0;
+		sum_acc[x] = 0;
+		pow_acc[x] = 0;
+	}
+
+	for(int r=0; r<ROW;r++){
+	#pragma HLS PIPELINE
+
+		f_col += lmask[r][0];;
+		f_pow += lmask[r][0]*lmask[r][0];
+
+		for(int c=0; c<80;c++){
+		#pragma HLS UNROLL factor=10
+
+			txm_acc[c] += (lt[r][c])*(lmask[r][c]);
+			sum_acc[c] += lmask[r][c];
+			pow_acc[c] += lmask[r][c]*lmask[r][c];
+
+		}
+	}
+
+	sum = reducesum(sum_acc,r_sum);
+	pow2 = reducesum(pow_acc,r_sum);
+	txm = reducesum(txm_acc,r_sum);
+
+}
+else{
+	sum -= f_col;
+	pow2 -= f_pow;
+	txm=0;
+	f_col=0;
+	f_pow=0;
+
+	for(int x=0;x<80;x++){
+#pragma HLS UNROLL factor=10
+			txm_acc[x] = 0;
+		}
+
+memcpy(tmp3,mask,sizeof(ap_uint<512>)*5);
+	for(int c=0;c<5;c++){
+//#pragma HLS UNROLL
+		ap_uint<512> a=tmp3[c];
+		for(int j=0;j<16;j++){
+#pragma HLS UNROLL
+			int upper = (j+1)*32-1;
+			int lower = j*32;
+
+			unsigned int m_tmp = a.range(upper,lower);
+			float m_val = *((float *)&m_tmp);
+			lcol[(c*16)+j]=m_val;
+		}
+	}
+
+	//memcpy(lcol,mask,sizeof(float)*COL);
+
+	for(int r=0;r<ROW;++r){
+#pragma HLS PIPELINE
+		for(int c=1;c<COL;c++){
+
+			lmask[r][c-1]=lmask[r][c];
+		}
+		lmask[r][COL-1]=lcol[r];
+
+	}
+
+
+	for(int r=0;r<ROW;r++){
+	#pragma HLS PIPELINE
+
+		f_col += lmask[r][0];
+		f_pow += lmask[r][0]*lmask[r][0];
+		sum+=lcol[r];
+		pow2+=lcol[r]*lcol[r];
+
+		for(int c=0;c<COL;c++){//9
+#pragma HLS UNROLL factor=10
+			txm_acc[c]+=lmask[r][c]*lt[r][c];
+
+
+		}
+
+	}
+
+		txm = reducesum(txm_acc,r_sum);
+
+}
+
+out_data.data=((N*txm)-(B*sum))/(D*(sqrt((N*pow2 - (sum*sum)))));
+out_data.last=finish;
+out.write(out_data);
 }
